36cb49de4e19d8e5586ddf3e2a87fe188a88b5c8
Penguin <chingiz.egamberdiev@testit.software>
2020-03-26 00:14:57 +0300

    delete custom-data

--- training/src/app/main/collections.component.ts
+++ training/src/app/main/collections.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-collections',
@@ -9,17 +9,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CollectionsComponent implements OnInit {
   public columnItems: string[] = ['one', 'two', 'three', 'four', 'five', 'six', 'seven'];
-  public collectionsCounter: number = 0;
+  public collectionsCounter = 0;
+  public url: ArrayBuffer | string = 'https://via.placeholder.com/150x216';
   public editor: FormGroup;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
     this.editor = new FormGroup({
       objectName: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required),
       customData: new FormControl('', Validators.required)
-    })
+    });
   }
 
   public ondrop(event: CdkDragDrop<string[]>) {
@@ -27,7 +29,18 @@ export class CollectionsComponent implements OnInit {
   }
 
   public readUrl(event) {
-    console.log(event);
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (progressEvent) => {
+        this.url = reader.result;
+      };
+    }
+
+    console.log(event.target);
+    console.log(event.target.files);
   }
 
 }
